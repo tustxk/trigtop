7bc97fe8dbba01d1ff4dceadd2fdb373542afab5
xiaoke <ke.xiao@trigtop.com>
2017-11-21 18:50:38 +0800

    add setupWizard
    frameworks/base
    packages/apps/trigtop
    vendor/realtek/apps/RealtekSettings

--- deviceTrigtop.mk
+++ deviceTrigtop.mk
@@ -3,3 +3,4 @@ include packages/apps/trigtop/sys_copy/sys_copy.mk
 PRODUCT_PACKAGES += AdupsFota
 PRODUCT_PACKAGES += AdupsFotaReboot
 PRODUCT_PACKAGES += Lighthome
+PRODUCT_PACKAGES += wizardv10-20170208
--- sys_app/Android.mk
+++ sys_app/Android.mk
@@ -1,5 +1,17 @@
 LOCAL_PATH := $(call my-dir)
 
+#wizardv10-20170208
+###########################################################
+include $(CLEAR_VARS)
+LOCAL_MODULE := wizardv10-20170208
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_CLASS := APPS
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
+###########################################################
+
 #all-makefiles-under,$(LOCAL_PATH)
 ###########################################################
 include $(call all-makefiles-under,$(LOCAL_PATH))
