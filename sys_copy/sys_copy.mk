b3c592d2e5e7c7dea413b2942c8bf6a02571742c
xiaoke <ke.xiao@trigtop.com>
2017-11-21 18:22:56 +0800

    add remote+
    device/realtek/kylin
    openwrt/build_dir/target-aarch64_cortex-a53+neon_glibc-2.19/linux-rtd1295_mnas_emmc/linux-4.1.7
    packages/apps/trigtop

--- sys_copy/sys_copy.mk
+++ sys_copy/sys_copy.mk
@@ -1,2 +1,3 @@
 PRODUCT_COPY_FILES += packages/apps/trigtop/sys_copy/system/vendor/trigtop/modules/netxeon_nixie/netxeon_nixie.ko:system/vendor/trigtop/modules/netxeon_nixie/netxeon_nixie.ko
-
+PRODUCT_COPY_FILES += packages/apps/trigtop/sys_copy/system/vendor/probox/sensors.kylin.so:system/lib/hw/sensors.kylin.so
+PRODUCT_COPY_FILES += packages/apps/trigtop/sys_copy/system/vendor/probox/virtualSensor:system/xbin/virtualSensor
