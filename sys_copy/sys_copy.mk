5cee88c287c1d9f37733614e7306378ec125b528
xiaoke <ke.xiao@trigtop.com>
2017-11-22 09:51:25 +0800

    add bootanimation.zip for beelink 6.0

--- sys_copy/sys_copy.mk
+++ sys_copy/sys_copy.mk
@@ -3,3 +3,4 @@ PRODUCT_COPY_FILES += packages/apps/trigtop/sys_copy/system/vendor/probox/sensor
 PRODUCT_COPY_FILES += packages/apps/trigtop/sys_copy/system/vendor/probox/virtualSensor:system/xbin/virtualSensor
 PRODUCT_COPY_FILES += packages/apps/trigtop/sys_copy/system/usr/keylayout/Vendor_0c45_Product_1109.kl:system/usr/keylayout/Vendor_0c45_Product_1109.kl
 PRODUCT_COPY_FILES += packages/apps/trigtop/sys_copy/system/usr/keylayout/Vendor_1997_Product_2433.kl:system/usr/keylayout/Vendor_1997_Product_2433.kl
+PRODUCT_COPY_FILES += packages/apps/trigtop/sys_copy/system/media/bootanimation.zip:system/media/bootanimation.zip
