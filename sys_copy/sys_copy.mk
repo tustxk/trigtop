0d571c042067518d0778de278a56ec5437e0a256
xiaoke <ke.xiao@trigtop.com>
2017-11-22 09:47:35 +0800

    add key layout file for reference 2.4G remote ime

--- sys_copy/sys_copy.mk
+++ sys_copy/sys_copy.mk
@@ -1,3 +1,4 @@
 PRODUCT_COPY_FILES += packages/apps/trigtop/sys_copy/system/vendor/trigtop/modules/netxeon_nixie/netxeon_nixie.ko:system/vendor/trigtop/modules/netxeon_nixie/netxeon_nixie.ko
 PRODUCT_COPY_FILES += packages/apps/trigtop/sys_copy/system/vendor/probox/sensors.kylin.so:system/lib/hw/sensors.kylin.so
 PRODUCT_COPY_FILES += packages/apps/trigtop/sys_copy/system/vendor/probox/virtualSensor:system/xbin/virtualSensor
+PRODUCT_COPY_FILES += packages/apps/trigtop/sys_copy/system/usr/keylayout/Vendor_0c45_Product_1109.kl:system/usr/keylayout/Vendor_0c45_Product_1109.kl
